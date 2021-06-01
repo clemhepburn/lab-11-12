ff036b6ae53df396c6856ac94868c61a4c6d2d08
Clem Hepburn <cooper.hepburn@pcc.edu>
2021-06-01 11:52:17 -0700

    create and style add input form

--- src/add/AddPage.js
+++ src/add/AddPage.js
@@ -4,14 +4,47 @@ import './AddPage.css';
 
 export default class AddPage extends Component {
   state = {
-    cats: []
+    name: '',
+    type: '',
+    url: '',
+    year: 0,
+    lives: 0,
+    isSidekick: false
   };
 
   render() {
     return (
-      <div className="AddPage">
+      <form className="AddPage">
+        <h2 className="page-title">Add a New Cat</h2>
+        <fieldset>
+          <label>
+            name:
+            <input type="text" required />
+          </label>
+          <label>
+            type:
+            <input type="text" required />
+          </label>
+          <label>
+            image:
+            <input type="text" required />
+          </label>
+          <label>
+            year:
+            <input type="number" required />
+          </label>
+          <label>
+            year:
+            <input type="number" min="0" max="9" placeholder="9" required />
+          </label>
+          <label>
+            sidekick?:
+            <input type="checkbox"/>
+          </label>
+        </fieldset>
+        <button type="submit">Add</button>
         
-      </div>
+      </form>
     );
   }
 }
